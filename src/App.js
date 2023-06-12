cbde174e76890e45b7b007b9d931800c90ce8b50
Apriana <aprianacahya04@gmail.com>
2023-06-12 17:44:59 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {Home,About,Contact,NoPages} from './component/pages'
+import Navbar from './component/navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router basename='Dummy-React-Router/'>
+        <Routes>
+          <Route element={<Navbar/>}>
+            <Route index element={<Home/>}/>
+            <Route path='About' element={<About/>}/>
+            <Route path='Contact' element={<Contact/>}/>
+            <Route path='*' element={<NoPages/>}/>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/component/pages.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Home = () => {
+  return (<h1>PAGE HOME</h1>)
+}
+
+const About = () => {
+  return (<h1>PAGE ABOUT</h1>)
+}
+const Contact = () => {
+  return (<h1>PAGE CONTACT</h1>)
+}
+const NoPages = () => {
+  return (<h1>404</h1>)
+}
+export {Home,About,Contact,NoPages};
+
